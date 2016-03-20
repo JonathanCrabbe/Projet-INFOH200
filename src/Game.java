c67924c07ec7387cc48a073ff7953223d7292852
Targamas <jonathan.cr1302@gmail.com>
2016-03-20 17:57:44 +0100

    Ajout Game et Window

--- src/Game.java
+++ src/Game.java
@@ -1,121 +1,106 @@
 import java.awt.Canvas;
-import java.awt.Dimension;
-import java.awt.Graphics2D;
-import java.awt.event.MouseAdapter;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+public class Game extends Canvas implements Runnable{
 
+	private static final long serialVersionUID = -6112428091888191314L;
+	
+	private Thread thread;
+	private boolean running = false;
+	
+	public static final int WIDTH = 640;
+	public static final int HEIGHT = WIDTH/12*9;
 
-public class Game implements Runnable{
-   
-   final int WIDTH = 1000;
-   final int HEIGHT = 700;
-   
-   JFrame frame;
-   Canvas canvas;
-   BufferStrategy bufferStrategy;
-   
-   public Game(){
-      frame = new JFrame("Basic Game");
-      
-      JPanel panel = (JPanel) frame.getContentPane();
-      panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-      panel.setLayout(null);
-      
-      canvas = new Canvas();
-      canvas.setBounds(0, 0, WIDTH, HEIGHT);
-      canvas.setIgnoreRepaint(true);
-      
-      panel.add(canvas);
-      
-      canvas.addMouseListener(new MouseControl());
-      
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.pack();
-      frame.setResizable(false);
-      frame.setVisible(true);
-      
-      canvas.createBufferStrategy(2);
-      bufferStrategy = canvas.getBufferStrategy();
-      
-      canvas.requestFocus();
-   }
-   
-        
-   private class MouseControl extends MouseAdapter{
-      
-   }
-   
-   long desiredFPS = 60;
-    long desiredDeltaLoop = (1000*1000*1000)/desiredFPS;
-    
-   boolean running = true;
-   
-   public void run(){
-      
-      long beginLoopTime;
-      long endLoopTime;
-      long currentUpdateTime = System.nanoTime();
-      long lastUpdateTime;
-      long deltaLoop;
-      
-      while(running){
-         beginLoopTime = System.nanoTime();
-         
-         render();
-         
-         lastUpdateTime = currentUpdateTime;
-         currentUpdateTime = System.nanoTime();
-         update((int) ((currentUpdateTime - lastUpdateTime)/(1000*1000)));
-         
-         endLoopTime = System.nanoTime();
-         deltaLoop = endLoopTime - beginLoopTime;
-           
-           if(deltaLoop > desiredDeltaLoop){
-               //Do nothing. We are already late.
-           }else{
-               try{
-                   Thread.sleep((desiredDeltaLoop - deltaLoop)/(1000*1000));
-               }catch(InterruptedException e){
-                   //Do nothing
-               }
-           }
-      }
-   }
-   
-   private void render() {
-      Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
-      g.clearRect(0, 0, WIDTH, HEIGHT);
-      render(g);
-      g.dispose();
-      bufferStrategy.show();
-   }
-   
-   //TESTING
-   private double x = 0;
-   
-   /**
-    * Rewrite this method for your game
-    */
-   protected void update(int deltaTime){
-      x += deltaTime * 0.2;
-      while(x > 500){
-         x -= 500;
-      }
-   }
-   
-   /**
-    * Rewrite this method for your game
-    */
-   protected void render(Graphics2D g){
-      g.fillRect((int)x, 0, 200, 200);
-   }
-   
-   public static void main(String [] args){
-      Game ex = new Game();
-      new Thread(ex).start();
-   }
+	
+	
+	public Game(){
+		new Window(WIDTH, HEIGHT, "Rogue Heritage", this);
+	}
 
+	public synchronized void start(){
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+		
+	}
+	
+	public synchronized void stop(){
+		try{
+			// Tue le thread
+			thread.join();
+			running = false;
+			
+		}catch(Exception e){
+			e.printStackTrace();  //Affiche erreur dans la console
+		}
+		
+	}
+	
+	public void run(){
+		
+		/* 
+			
+			Game loop pour réguler l'exécution des tâches.
+			Récupérée sur: https://www.youtube.com/watch?v=1gir2R7G9ws 
+		
+		
+		*/
+		
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000/ amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running){
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			
+			while(delta >= 1){
+				tick();
+				delta--;
+			}
+			if(running){
+				render();
+			}
+			frames++;
+			
+			if(System.currentTimeMillis()- timer > 1000){
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();
+		
+	}
+	
+	private void tick(){
+		
+	}
+	
+	private void render(){
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null){
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, WIDTH/2, HEIGHT);
+		
+		g.dispose();
+		bs.show();
+	}
+	
+	
+	public static void main(String args[]){
+			new Game();
+		
+	}
 }
